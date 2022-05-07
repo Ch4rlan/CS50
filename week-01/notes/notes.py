0207d2b30eccedc896885377a594a2e25c803de3
Guilherme <guiroli123@outlook.com>
2022-05-07 15:21:09 -0300

    adiciona notas

---
+++ week-01/notes/notes.py
@@ -0,0 +1,20 @@
+#  Challenge 01 ==>
+#  How to transform a decimal number in a binary?
+#  e.d. transform 65 in 01000001
+#  character-encoding: ASCII/UTF-8
+
+#  Challenge 02 ==>
+#  How to transform a number in ASCII/UTF-8 symbol?
+
+#  Challenge 03 ==>
+#  How to encrypt and decrypt a word using XOR Swap and binary?
+#  Search for XOR Swap if necessary.
+
+#  HELLO WORD BINARY:
+#  01001000 01000101 01001100 01001100 01001111 |
+
+#  SCIENCE CRYPTO BINARY:
+#  01010011 01000011 01001001 01000101 01001110 | 01000011 01000101
+
+#  ENCRYPTED WORD:
+#  00000010 00000101 00001110 00000000
